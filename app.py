e89a91654ff4c0d57e515b566d376694aa8dae91
Taksha Thosani <taksha.27@gmail.com>
2023-06-12 16:36:10 -0500

    first

--- app.py
+++ app.py
@@ -21,101 +21,147 @@ def data():
                 data.append(r)
         return render_template('data.html')
 
-@app.route("/teln", methods=['GET', 'POST'])
-def teln():
+@app.route("/rown", methods=['GET', 'POST'])
+def rown():
     if request.method == 'POST':
-        teln = request.form['teln']
-        csv_reader = csv.DictReader(open('static/q0c.csv'))
+        rown = request.form['rown']
+        csv_reader = csv.DictReader(open('static/q1c.csv'))
         matching_records = []
         for r in csv_reader:
-            if teln == r['teln']:
+            if rown == r['row']:
                 matching_records.append({
                     'image_path': '../static/' + r['pic'],
                     'name': r['name'],
-                    'description': r['descript']
+                    'seatno': r['seat']
                 })
 
         if matching_records:
-            return render_template('teln.html', records=matching_records, message="found")
+            return render_template('rown.html', records=matching_records, message="found")
         else:
-            return render_template('teln.html', error="Picture and Name did not find for Room!")
+            return render_template('rown.html', error="Picture and Name did not find for Room!")
 
 
 @app.route("/rnrange", methods=['GET', 'POST'])
-def roomnorange():
+def rownorange():
     if request.method == 'POST':
         rn1 = request.form['rn1']
         rn2 = request.form['rn2']
-        csv_reader = csv.DictReader(open('static/q0c.csv'))
+        seatlet = request.form['seatl']
+        csv_reader = csv.DictReader(open('static/q1c.csv'))
         matching_records = []
         for r in csv_reader:
-            if rn1 >= r['room'] or rn2 <= r['room']:
+            
+            if rn1 !='' and rn2 !='':
+                if seatlet!='':
+                    if r['row'] >= rn1 and r['row'] <= rn2 or r['seat'] == seatlet:
+                        matching_records.append({
+                            'image_path': '../static/' + r['pic'],
+                            'name': r['name'],
+                            'seatno': r['seat'],
+                            'notesdesc' : r['notes']
+                        })
+                elif r['row'] >= rn1 and r['row'] <= rn2:
+                     matching_records.append({
+                            'image_path': '../static/' + r['pic'],
+                            'name': r['name'],
+                            'seatno': r['seat'],
+                            'notesdesc' : r['notes']
+                        })   
+
+            elif r['seat'] == seatlet :
                 matching_records.append({
                     'image_path': '../static/' + r['pic'],
                     'name': r['name'],
-                    'description': r['descript']
-                })
+                    'seatno': r['seat'],
+                    'notesdesc' : r['notes']
+                })   
 
         if matching_records:
             return render_template('rnrange.html', records=matching_records, message="found")
         else:
             return render_template('rnrange.html', error="Picture and Name did not find for the given Room no!")
+        
+
+@app.route("/edit", methods=['GET', 'POST'])
+def edit_details_by_name():
+    return render_template('edit_details_by_name.html')
 
 
-# @app.route("/room", methods=['GET', 'POST'])
-# def update_desc():
-#     if request.method == 'POST':
-#         nm = request.form['name']
-#         upddesc = request.form['updated_desc']
-#         csv_reader = csv.DictReader(open('static/q0c.csv'))
-#         temp_name = ''
-#         temp_descript1 = ''
-#         for r in csv_reader:
-#             if nm == r['name']:
-#                 temp_name = r['name']
-#                 r['descript'] = upddesc
-#                 temp_descript1 = r['descript']
-#                 return render_template('room.html', name=temp_name, description=temp_descript1,
-#                                        message="found")
-
-#         if temp_name == '' or temp_descript1 == '':
-#             return render_template('room.html', error="Picture and Name did not find for Room!")
-
-
-@app.route("/room", methods=['GET', 'POST'])
-def update_desc():
+@app.route("/editdetails", methods=['GET', 'POST'])
+def editdetails_form():
     if request.method == 'POST':
-        nm = request.form['name']
-        upddesc = request.form['updated_desc']
-        csv_file = 'static/q0c.csv'
-        temp_file = tempfile.NamedTemporaryFile(mode='w', delete=False)
+        name = request.form['name']
+        csv_reader = csv.DictReader(open('static/q1c.csv'))
+        temp_name = ''
+        for r in csv_reader:
+            if name == r['name']:
+                temp_name = name
+        if temp_name != '':
+            return render_template('display_details_after_edit_by_name.html', name=temp_name)
+        else:
+            return render_template('display_details_after_edit_by_name.html', error="No Record Found!")
 
-        with open(csv_file, 'r') as file:
-            csv_reader = csv.DictReader(file)
-            fieldnames = csv_reader.fieldnames
-            writer = csv.DictWriter(temp_file, fieldnames=fieldnames)
+
+@app.route("/updatedetails", methods=['GET', 'POST'])
+def display_updated_details():
+    if request.method == 'POST':
+        name = request.form['name']
+        row = request.form['row']
+        seat = request.form['seat']
+        pic = request.files['pic']  
+        notes = request.form['notes']
+        cnt = 0
+
+        temp = [name, row, seat,pic.filename,notes]  
+        line = []
+
+        with open('static/q1c.csv', 'r') as f1:
+            csv_reader = csv.reader(f1)
             for r in csv_reader:
-                if nm == r['name']:
-                    r['descript'] = upddesc
-                writer.writerow(r)
+                if name == r[0]:
+                    line.append(temp)
+                else:
+                    line.append(r)
+                cnt += 1
 
-        shutil.move(temp_file.name, csv_file)
+        with open('static/q1c.csv', 'w') as csv_write:  
+            csv_writer = csv.writer(csv_write)
+            csv_writer.writerows(line)
 
-        with open(csv_file, 'r') as file:
-            csv_reader = csv.DictReader(file)
-            temp_name = ''
-            temp_descript1 = ''
-            for r in csv_reader:
-                if nm == r['name']:
-                    temp_name = r['name']
-                    temp_descript1 = r['descript']
-                    break
+        if cnt != 0:
+            return render_template('display_details_after_edit_by_name.html', update="One Record Updated Successfully.")
+        else:
+            return render_template('display_details_after_edit_by_name.html', error="No Record Found!")
+        
+@app.route("/remove", methods=['GET', 'POST'])
+def remove_details_by_name():
+    return render_template('remove_by_name.html')
 
-            if temp_name != '' and temp_descript1 != '':
-                return render_template('room.html', name=temp_name, description=temp_descript1, message="found")
-            else:
-                return render_template('room.html', error="Picture and Name did not find for Room!")
 
+@app.route("/removedetails", methods=['GET', 'POST'])
+def remove_details_message_display():
+    if request.method == 'POST':
+        name = request.form['name']
+        cnt = 0
+        line = list()
+        with open('static/q1c.csv', 'r') as f1:
+            csv_reader = csv.reader(f1)
+            for row in csv_reader:
+                line.append(row)
+                if name == row[0]:
+                    line.remove(row)
+                    cnt += 1
+
+        csv_write = open('static/q1c.csv', 'w')
+        for i in line:
+            for j in i:
+                csv_write.write(j + ',')
+            csv_write.write('\n')
+
+        if cnt:
+            return render_template('removedetails_validation.html', message="Record removed successfully.")
+        else:
+            return render_template('removedetails_validation.html', error="Record not found.")
 
 if __name__ == "__main__":
     app.run(debug=True)
